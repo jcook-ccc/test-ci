ea71016fcc8cc5363719282ca7d3bff9eb695128
Jordan Cook <jordan.cook@cccga.com>
2019-02-06 16:50:18 -0500

    added todo model and autoload for form

--- application/models/todo_model.php
+++ application/models/todo_model.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class Todo_model extens CI_Model {
+/* https://selftaughtcoders.com/build-a-simple-codeigniter-app/ */
+class Todo_model extends CI_Model {
 
   // General model constructor function
   function __construct() {
@@ -9,11 +9,23 @@ class Todo_model extens CI_Model {
 
   // Getting all the rows in the todos table
   function get_all_entries() {
-    $query = $this->db->get('todos');
+    $query = $this->db->order_by('order', 'ASC')->get('todos');
     $results = array();
     foreach ($query->result() as $result) {
       $results[] = $result;
     }
     return $results;
   }
+
+  // Get completed entries
+/*  function get_completed_entries() {
+    $query = $this->db->get_where('todos', array('complete'=>1));
+    $results = array();
+    foreach ($query->result() as $result) {
+      $results[] = $result;
+    }
+    return $results;
+  }*/
+
+  function update_entries($update_info, )
 }
